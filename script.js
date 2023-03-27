f6a611ee66f3fa2fb325ce23550d7a7d707dcb5f
Rodrigo Silveira <rodrigo.garra77@hotmail.com>
2023-03-27 11:55:07 -0300

    html and css

---
+++ script.js
@@ -0,0 +1,8 @@
+const form = document.getElementById ('formContacts')
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const inputNameContact= document.getElementById('nameContact');
+    const inputnumberContact= document.getElementById('numberContact');
+}); 
