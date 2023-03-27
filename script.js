df3ff319f7690a7a0c77210010b934199780def5
Rodrigo Silveira <rodrigo.garra77@hotmail.com>
2023-03-27 16:04:59 -0300

    tentativa-script

--- script.js
+++ script.js
@@ -6,3 +6,105 @@ form.addEventListener('submit', function(e){
     const inputNameContact= document.getElementById('nameContact');
     const inputnumberContact= document.getElementById('numberContact');
 }); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const form = document.getElementById('formContacts');
+const contacts = [];
+const numbers = [];
+
+
+let linhas = '';
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    adicionaLinha();
+    atualizaTabela();
+});             
+
+
+function adicionaLinha (){
+    const inputNameContact= document.getElementById('nameContact');
+    const inputNumberContact= document.getElementById('numberContact');
+
+    if(contacts.includes(inputNameContact.value)) {
+        alert(`O contato ${inputNameContact.value} ja foi inserido!`)
+    } else {
+
+        contacts.push(inputNameContact.value);
+        numbers.push(parseFloat(inputNumberContact.value));
+    
+    let linha = "<tr>";
+    linha += `<td>${inputNameContact.value}</td>`;
+    linha += `<td>${inputNumberContact.value}</td>`;
+    linha += `<td>${inputNumberContact.value >= 11 ? 'Numero correto' : 'Numero incorreto'}</td>`;
+    linha += "</tr>";
+
+    linhas += linha;
+    }
+
+    inputNomeAtividade.value ='';
+    inputNotaAtividade.value='';
+}
+
+function atualizaTabela(){
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
+*/
